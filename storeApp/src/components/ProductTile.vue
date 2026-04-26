72714e5bf1ef20cee7d9f6c2405179856c604ac1
bk <bkosciel@interia.pl>
2026-04-26 18:41:10 +0200

    juz prawie 5 czesc :))

--- storeApp/src/components/ProductTile.vue
+++ storeApp/src/components/ProductTile.vue
@@ -1,38 +1,4 @@
-<!-- <template>
-    <div class="product-tile">        
-        <RouterLink :to="`/product/${this.product.id}`">
-            <img :src=getUrl(product.image) class="img">
-            <div>{{ product.name }}</div>
-            <div>{{ product.price }}$</div>
-        </RouterLink>
-    </div>
-</template>
 
-<script>
-export default {
-    props: { product: Object },
-    methods: {
-        getUrl(img){
-            return `/src/assets/${img}`
-        }
-    }
-}
-</script>
-
-<style lang="css" scoped>
-.img{
-    width: 200px;
-    height: 200px;
-}
-.product-tile{
-    margin: 10px;
-    padding: 10px;
-    border: 1px solid gray;
-    border-radius: 10px;
-}
-
-
-</style> -->
 <template>
 	<div class="product-tile">
 		<RouterLink :to="`/product/${product.id}`">
@@ -49,7 +15,7 @@ export default {
 </template>
 
 <script>
-// Zakładamy, że AppLoader jest zarejestrowany globalnie lub importowany tutaj
+
 import AppLoader from '@/components/AppLoader.vue'
 
 export default {
@@ -61,18 +27,18 @@ export default {
 	},
 	data() {
 		return {
-			loading: true, // Loader widoczny na starcie
-			imageLoaded: false, // Flaga stanu obrazka
+			loading: true, 
+			imageLoaded: false,
 		}
 	},
 	computed: {
-		// Dynamiczny styl tła dla diva
+
 		imageStyle() {
 			return {
 				backgroundImage: this.imageLoaded ? `url(${this.getUrl(this.product.image)})` : 'none',
 				backgroundSize: 'cover',
 				backgroundPosition: 'center',
-				position: 'relative', // Ważne dla pozycjonowania loadera (absolute)
+				position: 'relative',
 			}
 		},
 	},
