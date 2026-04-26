1ea0b4cfe81fef421e72416c2bde817cf4012449
bk <bkosciel@interia.pl>
2026-04-26 17:40:15 +0200

    hot fix

--- storeApp/src/components/AppLoader.vue
+++ storeApp/src/components/AppLoader.vue
@@ -1,24 +1,46 @@
 <template>
-    <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
-            <rect fill="#FF156D" stroke="#FF156D" stroke-width="15" stroke-linejoin="round" width="30" height="30"
-                x="85" y="85" rx="0" ry="0">
-                <animate attributeName="rx" calcMode="spline" dur="2" values="15;15;5;15;15"
-                    keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate>
-                <animate attributeName="ry" calcMode="spline" dur="2" values="15;15;10;15;15"
-                    keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate>
-                <animate attributeName="height" calcMode="spline" dur="2" values="30;30;1;30;30"
-                    keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate>
-                <animate attributeName="y" calcMode="spline" dur="2" values="40;170;40;"
-                    keySplines=".6 0 1 .4;0 .8 .2 1" repeatCount="indefinite"></animate>
-            </rect>
-        </svg>
+  <div class="loader-overlay">
+    <div class="loader-container">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+        <rect fill="#FF156D" stroke="#FF156D" stroke-width="15" stroke-linejoin="round" width="30" height="30"
+          x="85" y="85" rx="0" ry="0">
+          <animate attributeName="rx" calcMode="spline" dur="2" values="15;15;5;15;15"
+            keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate>
+          <animate attributeName="ry" calcMode="spline" dur="2" values="15;15;10;15;15"
+            keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate>
+          <animate attributeName="height" calcMode="spline" dur="2" values="30;30;1;30;30"
+            keySplines=".5 0 .5 1;.8 0 1 .2;0 .8 .2 1;.5 0 .5 1" repeatCount="indefinite"></animate>
+          <animate attributeName="y" calcMode="spline" dur="2" values="40;170;40;"
+            keySplines=".6 0 1 .4;0 .8 .2 1" repeatCount="indefinite"></animate>
+        </rect>
+      </svg>
     </div>
+  </div>
 </template>
 
-<script>
-export default {
+<style scoped>
+.loader-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  border-radius: inherit;
 }
-</script>
 
-<style lang="scss" scoped></style>
+.loader-container {
+  width: 100px;
+  height: 100px;
+}
+
+.loader-container svg {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+</style>
--- storeApp/src/components/Header.vue
+++ storeApp/src/components/Header.vue
@@ -1,31 +1,37 @@
 <template>
-    <AppLoader v-show="userLoading" />
 	<div class="nav">
 		<div class="routing">
+			<AppLoader v-show="userLoading" />
 			<RouterLink class="nav-element" to="/" exact>Home</RouterLink>
 			<RouterLink class="nav-element" to="/about">About</RouterLink>
 			<RouterLink class="nav-element" to="/nnn">Not found</RouterLink>
 		</div>
-		<div class="register-header" v-show="!user">
+        <AppLoader v-show="userLoading" />
+		<div class="register-header" v-show="!user && !userLoading">
 			<RouterLink class="" to="/register"><button>Register</button></RouterLink>
 			<RouterLink class="" to="/login"><button>Login</button></RouterLink>
 		</div>
-		<div class="register-header" v-show="user">
+		<div class="register-header" v-show="user && !userLoading">
+			<AppLoader v-show="userLoading" />
+			<span>Welcome, {{ user }}!</span>
 			<button class="nav-element" @click="logout">Logout</button>
 		</div>
 	</div>
 </template>
 
 <script>
-import AppLoader from '@/components/AppLoader.vue';
+import AppLoader from '@/components/AppLoader.vue'
 
 export default {
 	components: {
-		AppLoader
+		AppLoader,
 	},
 	computed: {
 		user() {
-			return this.$store.getters.GET_CURRENT_USER
+			if (this.$store.getters.GET_CURRENT_USER != null) return this.$store.getters.GET_CURRENT_USER.email
+			else {
+				return null
+			}
 		},
 		userLoading() {
 			return this.$store.getters.GET_CURRENT_USER_LOADING
@@ -43,29 +49,33 @@ export default {
 
 <style lang="css" scoped>
 .nav {
-	display: flex;
-	justify-content: space-between;
-	height: 50px;
-	width: 100%;
-	display: flex;
-	align-items: center;
-	background-color: black;
+    display: flex;
+    justify-content: space-between;
+    height: 50px;
+    width: 100%;
+    align-items: center;
+    background-color: black;
 }
+
 .routing {
-	display: flex;
+    display: flex;
 }
-.nav-element:hover {
-	background-color: #333;
-	color: rgb(255, 8, 8);
-}
+
 .nav-element {
-	padding: 5px 30px;
+    padding: 5px 30px;
 }
+
+.nav-element:hover {
+    background-color: #333;
+    color: rgb(255, 8, 8);
+}
+
 a {
-	text-decoration: none;
-	color: #f6ff00;
+    text-decoration: none;
+    color: #f6ff00;
 }
-button{
+
+button {
     background-color: transparent;
     border: 1px solid #f6ff00;
     color: #f6ff00;
@@ -73,8 +83,23 @@ button{
     cursor: pointer;
     margin-left: 5px;
 }
-button:hover{
+
+button:hover {
     background-color: #f6ff00;
     color: black;
+}
+
+.register-header {
+    position: relative; /* Kluczowe dla loadera */
+    display: flex;
+    align-items: center;
+    height: 100%; /* Wypełnia wysokość .nav */
+    padding: 0 10px;
+    overflow: hidden; /* Utrzymuje tło loadera w ryzach */
+}
+
+.register-header span {
+    color: #f6ff00;
+    margin-right: 10px;
 }
 </style>
